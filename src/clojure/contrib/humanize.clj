f5257974edb4886a27cd1fa54e7a3380847523e1
Thura Hlaing <trhura@gmail.com>
2014-01-21 22:42:37 +0630

    refactored code to use in? predicate

--- src/clojure/contrib/humanize.clj
+++ src/clojure/contrib/humanize.clj
@@ -1,10 +1,17 @@
 (ns clojure.contrib.humanize)
 
-(defn intcomma [num]
-  """ Converts an integer to a string containing commas.
-      every three digits. For example, 3000 becomes '3,000'
-      and 45000 becomes '45,000'. """
+(defn in? [x coll]
+  """ Return true if x is in coll, else false. """
+  (if (empty? coll)
+    false
+    (if (= x (first coll))
+      true
+      (in? x (rest coll)))))
 
+
+(defn intcomma [num]
+  """ Converts an integer to a string containing commas. every three digits.
+      For example, 3000 becomes '3,000' and 45000 becomes '45,000'. """
   (let [;; convert into string representation
         repr (str num)
         repr-len (count repr)
@@ -15,12 +22,24 @@
                           (partition 3 (subs repr
                                              (rem repr-len 3))))]
 
-        ;; remove empty "", and flatten
-        partitioned (flatten (remove empty? partitioned))]
+        ;; flatten
+        partitioned (flatten partitioned)]
 
-    (if (= (first partitioned) "-")
-      ;; skip the first if it is only negative sign
+    (if (in? (first partitioned) ["" "-"])
+      ;; skip the first if it is negative sign or empty
       (apply str (first partitioned)
              (interpose "," (rest partitioned)))
-      ;; interpose the rest with comma
+      ;; else interpose all
       (apply str (interpose "," partitioned)))))
+
+
+(defn ordinal [num]
+  """Converts an integer to its ordinal as a string. 1 is '1st', 2 is '2nd',
+    3 is '3rd', etc."""
+    (let [ordinals ["th", "st", "nd", "rd", "th",
+                    "th", "th", "th", "th", "th"]]
+
+      (if (in? (rem num 100) [11 12 13])
+        ;; special case for *11, *12, *13
+        (str num (ordinals 0))
+        (str num (ordinals (rem num 10))))))
--- test/clojure/contrib/humanize_test.clj
+++ test/clojure/contrib/humanize_test.clj
@@ -9,5 +9,15 @@
                               [1000000, "1,000,000"], [-100, "-100"],
                               [-10123 "-10,123"], [-10311 "-10,311"],
                               [-1000000, "-1,000,000"]]]
-            (is (= (intcomma testnum) result))))
-    )
+      (is (= (intcomma testnum) result))))
+
+  (testing "Testing ordinal function."
+    (doseq [[testnum result] [[1,"1st"], [ 2,"2nd"],
+                              [ 3,"3rd"], [ 4,"4th"],
+                              [ 11,"11th"],[ 12,"12th"],
+                              [ 13,"13th"], [ 101,"101st"],
+                              [ 102,"102nd"], [ 103,"103rd"],
+                              [111, "111th"]]]
+      (is (= (ordinal testnum) result))))
+
+  )
