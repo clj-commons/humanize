3855913e79aaf02b0a8ef34d74acdcecc4d7c0bc
Thura Hlaing <trhura@gmail.com>
2014-01-22 21:19:54 +0630

    implemented intword function

--- src/clojure/contrib/humanize.clj
+++ src/clojure/contrib/humanize.clj
@@ -41,22 +41,59 @@
         (str num (ordinals 0))
         (str num (ordinals remainder-10)))))
 
-(defn- logn [num base]
-  (/ (java.lang.Math/log num)
-     (java.lang.Math/log base)))
+(defn logn [num base]
+  (/ (round (java.lang.Math/log num))
+     (round (java.lang.Math/log base))))
 
-(def decimal-sizes  [:B, :KB, :MB, :GB, :TB, :PB, :EB, :ZB, :YB])
-(def binary-sizes [:B, :KiB, :MiB, :GiB, :TiB, :PiB, :EiB, :ZiB, :YiB])
+(defn intword [num & {:keys [format] :or {format "%.1f"}}]
+  """Converts a large integer to a friendly text representation. Works best for
+    numbers over 1 million. For example, 1000000 becomes '1.0 million', 1200000
+    becomes '1.2 million' and '1200000000' becomes '1.2 billion'.  Supports up to
+    decillion (33 digits) and googol (100 digits)."""
+  (let [human-pows {
+                    0 "",
+                      6 " million",
+                      9  " billion",
+                      12  " trillion",
+                      15  " quadrillion",
+                      18 " quintillion",
+                      21  " sextillion",
+                      24  " septillion",
+                      27 " octillion",
+                      30  " nonillion",
+                      33  " decillion",
+                      100 " googol",
+                      }
 
-(defn filesize [bytes &
-                {:keys [binary format]
-                 :or {binary false
-                      format "%.1f"
-                      }}]
-  (let [units (if binary binary-sizes decimal-sizes)
+        base-pow  (int (floor (java.lang.Math/log10 num)))
+        base-pow  (if (contains? human-pows base-pow)
+                    base-pow
+                    (last (remove #(> % base-pow) (sort (keys human-pows)))))
+
+        suffix (get human-pows base-pow)
+        value (float (/ num (expt 10 base-pow)))
+        ]
+    (clojure.core/format (str format "%s") value suffix)
+    ))
+
+
+(defn filesize [bytes & {:keys [binary format]
+                         :or {binary false
+                              format "%.1f"}}]
+  """ Format a number of byteslike a human readable filesize (eg. 10 kB).  By
+    default, decimal suffixes (kB, MB) are used.  Passing binary=true will use
+    binary suffixes (KiB, MiB) are used"""
+
+  (let [decimal-sizes  [:B, :KB, :MB, :GB, :TB,
+                        :PB, :EB, :ZB, :YB]
+        binary-sizes [:B, :KiB, :MiB, :GiB, :TiB,
+                      :PiB, :EiB, :ZiB, :YiB]
+
+        units (if binary binary-sizes decimal-sizes)
         base  (if binary 1024 1000)
 
         base-pow  (int (floor (logn bytes base)))
+        ;; if base power shouldn't be larger than biggest unit
         base-pow  (if (< base-pow (count units))
                     base-pow
                     (dec (count units)))
