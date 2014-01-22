adb0c953e79838f1f7297daa7ba16b86495a36b5
Thura Hlaing <trhura@gmail.com>
2014-01-22 16:29:09 +0630

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-humanize "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "produce human readable strings in clojure"
+  :url "https://github.com/trhura/clojure-humanize"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
