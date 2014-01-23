28003ecc1297a1c84701c8dac1872773cfc67b52
Thura Hlaing <trhura@gmail.com>
2014-01-23 13:35:16 +0630

    added clj-time dep

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 ])
+                 [clj-time "0.6.0"]
+                 ]){}
