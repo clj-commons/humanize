51a3959db753302d15415ba45e058d12b607267f
Thura Hlaing <trhura@gmail.com>
2014-01-24 21:39:10 +0630

    added tests for oxford function

--- test/clojure/contrib/humanize_test.clj
+++ test/clojure/contrib/humanize_test.clj
@@ -77,3 +77,41 @@
     (let [string "abcdefghijklmnopqrstuvwxyz"]
       (is (= (truncate string 14) "abcdefghijk..."))
       (is (= (truncate string 14 "...kidding") "abcd...kidding")))))
+
+(deftest oxford-test
+  (let [items ["apple", "orange", "banana", "pear", "pineapple", "strawberry"]]
+    (testing "should return an empty string when given an empty list."
+      (is (= (oxford []) "")))
+
+    (testing "should return a string version of a list that has only one value."
+      (is (= (oxford [(items 0)]) (items 0))))
+
+    (testing "should return items separated by `and' when given a list of values"
+      (is (= (oxford (take 2 items)) (str (items 0) " and " (items 1))))
+      (is (= (oxford (take 3 items)) (str (items 0) ", "
+                                          (items 1) " and " (items 2))))
+      (is (= (oxford (take 4 items)) (str (items 0) ", "
+                                          (items 1) ", "
+                                          (items 2) " and " (items 3)))))
+
+    (testing "should truncate a large list of items with proper pluralization"
+      (is (= (oxford (take 5 items)) (str (items 0) ", "
+                                          (items 1) ", "
+                                          (items 2) ", "
+                                          (items 3) " and " 1 " other")))
+      (is (= (oxford (take 5 items)
+                     :maximum-display 2)
+             (str (items 0) ", "
+                  (items 1) " and " 3 " others"))))
+
+    (testing "should accept custom trucation strings"
+      (let [truncate-noun "fruit"]
+        (is (oxford (take 5 items)
+                    :truncate-noun truncate-noun)
+            (str (items 0) ", "
+                 (items 1) ", "
+                 (items 2) " and " 2 " other " (pluralize 2 truncate-noun)))
+        (is (oxford (take 3 items)
+                    :truncate-string truncate-noun)
+            (str (items 0) ", "
+                 (items 1) " and " (items 2)))))))
