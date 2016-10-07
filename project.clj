b24addf4f2f87ff24f7126c70cda22b4f85b01b8
Kostas Georgiadis <kongeor@gmail.com>
2016-10-07 22:16:48 +0300

    Version 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-humanize "0.2.1-SNAPSHOT"
+(defproject clojure-humanize "0.2.1"
   :description "produce human readable strings in clojure"
   :url "https://github.com/trhura/clojure-humanize"
   :license {:name "Eclipse Public License"
